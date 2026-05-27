dcaf910b73377d563d4525676dfbbde2a1af7e55
Ansh18Nimker <anshnimker2004@gmail.com>
2026-05-27 20:17:13 +0530

    Simplify Vercel Flask deployment

--- index.py
+++ index.py
@@ -394,9 +394,5 @@ def logout():
 
 
 # ---------------- RUN ----------------
-with app.app_context():
-    db.create_all()
-
 if __name__ == "__main__":
-    print(app.url_map)
     app.run(debug=True)
