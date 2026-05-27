16ca49cda98b7f418a62ac42bf475b7c368199db
Ansh18Nimker <anshnimker2004@gmail.com>
2026-05-27 19:44:55 +0530

    Add Vercel config and Flask setup

--- app.py
+++ app.py
@@ -394,11 +394,9 @@ def logout():
 
 
 # ---------------- RUN ----------------
+with app.app_context():
+    db.create_all()
+
 if __name__ == "__main__":
-
-    with app.app_context():
-        db.create_all()
-
     print(app.url_map)
-
     app.run(debug=True)
